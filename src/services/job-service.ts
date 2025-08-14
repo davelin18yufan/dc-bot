c7f89a346ec5a37a71bc27a32da362975250fb13
davelin18yufan <davelin30630@gmail.com>
2025-08-14 15:25:27 +0800

    [refactor] refactor linting style

--- src/services/job-service.ts
+++ src/services/job-service.ts
@@ -1,20 +1,17 @@
-import { CronExpressionParser } from 'cron-parser';
-import { DateTime } from 'luxon';
-import schedule from 'node-schedule';
-import { createRequire } from 'node:module';
+import { CronExpressionParser } from "cron-parser";
+import { DateTime } from "luxon";
+import schedule from "node-schedule";
 
-import { Logger } from './index.js';
-import { Job } from '../jobs/index.js';
-
-const require = createRequire(import.meta.url);
-let Logs = require('../../lang/logs.json');
+import { Job } from "@/jobs/index.js";
+import { Logger } from "@/services/index.js";
+import Logs from "~/lang/logs.json";
 
 export class JobService {
     constructor(private jobs: Job[]) {}
 
     public start(): void {
-        for (let job of this.jobs) {
-            let jobSchedule = job.runOnce
+        for (const job of this.jobs) {
+            const jobSchedule = job.runOnce
                 ? CronExpressionParser.parse(job.schedule, {
                       currentDate: DateTime.now()
                           .plus({ seconds: job.initialDelaySecs })
@@ -30,22 +27,22 @@ export class JobService {
             schedule.scheduleJob(jobSchedule, async () => {
                 try {
                     if (job.log) {
-                        Logger.info(Logs.info.jobRun.replaceAll('{JOB}', job.name));
+                        Logger.info(Logs.info.jobRun.replaceAll("{JOB}", job.name));
                     }
 
                     await job.run();
 
                     if (job.log) {
-                        Logger.info(Logs.info.jobCompleted.replaceAll('{JOB}', job.name));
+                        Logger.info(Logs.info.jobCompleted.replaceAll("{JOB}", job.name));
                     }
                 } catch (error) {
-                    Logger.error(Logs.error.job.replaceAll('{JOB}', job.name), error);
+                    Logger.error(Logs.error.job.replaceAll("{JOB}", job.name), error);
                 }
             });
             Logger.info(
                 Logs.info.jobScheduled
-                    .replaceAll('{JOB}', job.name)
-                    .replaceAll('{SCHEDULE}', job.schedule)
+                    .replaceAll("{JOB}", job.name)
+                    .replaceAll("{SCHEDULE}", job.schedule)
             );
         }
     }
--- src/triggers/trigger.ts
+++ src/triggers/trigger.ts
@@ -1,6 +1,6 @@
-import { Message } from 'discord.js';
+import { Message } from "discord.js";
 
-import { EventData } from '../models/internal-models.js';
+import { EventData } from "@/models/internal-models.js";
 
 export interface Trigger {
     requireGuild: boolean;
